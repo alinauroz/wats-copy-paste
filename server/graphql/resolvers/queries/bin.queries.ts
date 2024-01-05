4272bc14331278f2e9cdfebef1c6c523a5c9c0c9
nauroz <alinauroze@hotmail.com>
2024-01-05 12:35:56 +0500

    query

--- server/graphql/resolvers/queries/bin.queries.ts
+++ server/graphql/resolvers/queries/bin.queries.ts
@@ -1,5 +1,10 @@
+import prisma from '@/prisma/prisma';
 import { IGqlContext } from '@/types';
 
-export const bin = (_: unknown, args: unknown, { user }: IGqlContext) => {
-  return user;
+export const bin = (
+  _: unknown,
+  { id: i_id }: { id: string },
+  { user }: IGqlContext
+) => {
+  return prisma.bin.findFirst({ where: { i_id } });
 };
