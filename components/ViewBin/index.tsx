bab99a54f22c4bc5b1ebab32bab18c48c86b0f80
muhib551 <muhib551@gmail.com>
2024-01-05 19:32:06 +0500

    added the div in the view bin

--- components/ViewBin/index.tsx
+++ components/ViewBin/index.tsx
@@ -34,18 +34,14 @@ function ViewBin({ binId }: { binId: string }) {
     <div className="w-10/12 mx-auto">
       <div className="">
         <p className="text-white py-2 text-3xl font-semibold mt-10">View Bin</p>
-        <div className="my-4 flex justify-center items-center">
+        <div className="my-8 flex justify-center items-center">
           <img
             src={`https://www.wordstream.com/wp-content/uploads/2021/07/banner-ads-examples-ncino.jpg`}
             className="w-full md:w-4/5"
           />
         </div>
-        <div className="my-8">
-          <textarea
-            disabled
-            className="border-none w-full h-96 text-white text-lg rounded-lg p-4 focus:outline-[#0f3955] focus:border-none bg-[#0f3955]"
-            placeholder={data?.bin?.text}
-          />
+        <div className="w-full h-96 text-white text-lg rounded-lg p-4 bg-[#0f3955]">
+          {data?.bin?.text}
         </div>
       </div>
     </div>
